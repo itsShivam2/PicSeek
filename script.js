4aaabda6965ea9001c772fe433b0dce3bdbc3610
itsShivam2 <135848325+itsShivam2@users.noreply.github.com>
2024-02-04 15:52:54 +0530

    Added ligthbox

--- script.js
+++ script.js
@@ -38,6 +38,31 @@ fetchImages();
 const searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener("click", fetchImages);
 
+
+function openLightbox(image) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImage = document.getElementById("lightbox-image");
+  console.log("image clicked");
+  lightboxImage.src = image.urls.full;
+  lightbox.style.display = "flex";
+
+  const closeBtn = document.getElementById("close-btn");
+  closeBtn.style.display = "block";
+  const container = document.querySelector(".container");
+  container.style.padding = "0";
+  container.style.margin = "0";
+
+}
+
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none";
+
+  const closeBtn = document.getElementById("close-btn");
+  closeBtn.style.display = "none";
+}
+
 function displayImages(images) {
   const main = document.getElementById("main");
   main.innerHTML = "";
@@ -50,6 +75,8 @@ function displayImages(images) {
     imageElement.setAttribute("class", "image");
     imageElement.src = image.urls.regular;
     imageElement.alt = image.alt_description;
+
+    imageContainer.addEventListener("click", () => openLightbox(image));
 
     const overlay = document.createElement("div");
     overlay.setAttribute("class", "overlay");
@@ -64,6 +91,8 @@ function displayImages(images) {
 function prevPage() {
   if (page > 1) page = page - 1;
   fetchImages(page);
+
+  document.getElementById("main").scrollIntoView({ behavior: "smooth" });
 }
 const prevImages = document.querySelector("#prev");
 prevImages.addEventListener("click", prevPage);
@@ -71,6 +100,8 @@ prevImages.addEventListener("click", prevPage);
 function nextPage() {
   if (page < totalPages) page = page + 1;
   fetchImages(page);
+
+  document.getElementById("main").scrollIntoView({ behavior: "smooth" });
 }
 const nextImages = document.querySelector("#next");
 nextImages.addEventListener("click", nextPage);
